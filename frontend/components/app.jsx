22e74171a32ed0d198a8cc1c542e381fb224f9ae
Samuel Michael <samadammichael@yahoo.com>
2018-11-14 15:36:12 -0500

    greeting component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,20 +6,22 @@ import {
     Link,
     HashRouter
  } from 'react-router-dom';
-
+import GreetingContainer from './greeting/greeting_container';
  import SignUpFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
  const App = () => (
      <div>
          <header>
-             <h1>highARglyphs</h1>
+             <Link to='/' className='header-link'>
+             <h1 className="app-name">highARglyphs</h1>
+             </Link>
+             <GreetingContainer />
          </header>
-             <Link to="/login">Login</Link>
-             <Link to="/signup">Signup</Link>
          <Switch>
-         <Route path="/login" component={LoginFormContainer} />
-         <Route path="/signup" component={SignUpFormContainer} />
+         <AuthRoute exact path="/login" component={LoginFormContainer} />
+         <AuthRoute exact path="/signup" component={SignUpFormContainer} />
          </Switch>
      </div>
  );
