cc204f85a8211b3fa784ef5cd58f20b806e04325
Samuel Michael <samadammichael@yahoo.com>
2018-11-14 12:26:40 -0500

    fix root reducer again

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,19 +2,9 @@ import { combineReducers } from 'redux';
 import entitiesReducer from './entities_reducer';
 import sessionReducer from './session_reducer';
 
-<<<<<<< HEAD
-import session from './session_reducer';
-
-const rootReducer = combineReducers({
-    session
-});
-
-export default rootReducer;
-=======
     const rootReducer = combineReducers({
         entities: entitiesReducer,
         session: sessionReducer
     });
 
     export default rootReducer;
->>>>>>> auth
